d33ba78de8a93b4b59a0ffdfa671b91a5dbed560
Nicolas <nicolas@codemate.consulting>
2024-09-13 14:36:51 +0200

    add configuration to make map configurable

--- desktop/js/geoloc.js
+++ desktop/js/geoloc.js
@@ -4,12 +4,13 @@ $(function () {
     let map = L.map('map').setView([defaultLatitude, defaultLongitude], defaultZoom);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        minZoom: 1,
         maxZoom: 20,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
     geolocalisableItems.forEach(function (item) {
         const marker = L.marker([item.latitude, item.longitude]).addTo(map);
-        marker.bindPopup(item.name);
+        marker.bindPopup(`<h2>${item.name}</h2>`);
     });
 });
